8b838ff5dcbbfbd277502a491bb5c2186f332978
Lei Shi <me@leishi.io>
2021-11-17 23:52:06 +0100

    use route group

--- route/router.go
+++ route/router.go
@@ -13,6 +13,22 @@ import (
 	echoMw "github.com/labstack/echo/middleware"
 )
 
+func useAPIGroup(group *echo.Group) {
+	group.GET("", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "OK")
+	})
+}
+
+func handleWeb(e *echo.Echo) echo.HandlerFunc {
+	statikFS, err := fs.New()
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	h := http.FileServer(statikFS)
+
+	return echo.WrapHandler(http.StripPrefix("/", h))
+}
+
 func Init() *echo.Echo {
 
 	e := echo.New()
@@ -24,14 +40,17 @@ func Init() *echo.Echo {
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAcceptEncoding},
 	}))
+	// if username := viper.GetString("web.username"); username != "" {
+	// 	if passwordHash := viper.GetString("web.password"); passwordHash != "" {
+	// 		e.Use(echoMw.JWTWithConfig(echoMw.JWTConfig{
+	// 			SigningKey:  []byte(username + passwordHash),
+	// 			TokenLookup: "query:token",
+	// 		}))
+	// 	}
+	// }
 
-	statikFS, err := fs.New()
-	if err != nil {
-		e.Logger.Fatal(err)
-	}
-	h := http.FileServer(statikFS)
-
-	e.GET("/*", echo.WrapHandler(http.StripPrefix("/", h)))
+	useAPIGroup(e.Group("/api"))
+	e.GET("/*", handleWeb(e))
 
 	return e
 }
